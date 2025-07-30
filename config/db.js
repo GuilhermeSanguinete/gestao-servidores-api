94d34c38acac7a342bbfcfc1e51aa16b3f0aa586
Guilherme Sanguinete <santos.guilherme.dev@gmail.com>
2025-07-30 17:24:29 -0300

    chore: configura conexão com MySQL usando createPool e variáveis do .env

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD,
+    database: process.env.MYSQL_DB,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+console.log('Pool de conexões criado com sucesso!');
+
+export default pool;
