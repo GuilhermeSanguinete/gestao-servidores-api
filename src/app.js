747fe04e02572a81763ccf57b3bf626a2d26d4f7
Guilherme Sanguinete <santos.guilherme.dev@gmail.com>
2025-07-30 16:36:46 -0300

    feat: cria servidor Express básico com porta 3000 e CORS habilitado

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = 3000;
+app.listen(port, () => console.log(`Api subiu na porta ${port}`));
