a1de9534f9819cf8e602636f44cc37a644051e4f
Guilherme Sanguinete <santos.guilherme.dev@gmail.com>
2025-08-08 20:34:35 -0300

    fix(servidores): corrigir chamadas para servidoresService em todas as funções da controller

--- src/controllers/servidoresController.js
+++ src/controllers/servidoresController.js
@@ -1,8 +1,8 @@
-import * as servidoresSerice from '../services/servidoresServices.js';
+import * as servidoresService from '../services/servidoresServices.js';
 
 export async function getServidores(req, res) {
     try {
-        const resultado = await servidoresSerice.getServidores();
+        const resultado = await servidoresService.getServidores();
         res.status(200).json(resultado);
     }
     catch (error) {
@@ -13,7 +13,7 @@ export async function getServidores(req, res) {
 export async function postServidores(req, res) {
     try {
         const servidor = req.body;
-        const resultado = await servidoresSerice.postServidores(servidor);
+        const resultado = await servidoresService.postServidores(servidor);
         res.status(201).json(resultado);
     }
     catch (error) {
@@ -24,7 +24,7 @@ export async function postServidores(req, res) {
 export async function deleteServidores(req, res) {
     try {
         const id = req.body.id;
-        const resultado = await servidoresSerice.deleteServidores(id);
+        const resultado = await servidoresService.deleteServidores(id);
         res.status(200).json({ message: 'Servidor deletado com sucesso ' });
     }
     catch (error) {
@@ -35,7 +35,7 @@ export async function deleteServidores(req, res) {
 export async function putServidores(req, res) {
     try{
         const empresa = req.body;
-        await servidoresSerice.putServidores(empresa);
+        await servidoresService.putServidores(empresa);
         res.status(200).json({ message: 'Servidor atualizado com sucesso '});
     }
     catch (error) {
@@ -46,7 +46,7 @@ export async function putServidores(req, res) {
 export async function getServidorById(req, res) {
     try {
         const id = req.params.id;
-        const resultado = await servidoresSerice.getServidorById(id);
+        const resultado = await servidoresService.getServidorById(id);
         if (!resultado) {
             return res.status(404).json({ message: 'Servidor não encontrado' });
         }
@@ -59,7 +59,7 @@ export async function getServidorById(req, res) {
 export async function getServidoresByEmpresa(req, res) {
     try {
         const empresaId = req.params.empresaId;
-        const resultado = await servidoresSerice.getServidoresByEmpresa(empresaId);
+        const resultado = await servidoresService.getServidoresByEmpresa(empresaId);
         res.status(200).json(resultado);
     } catch (error) {
         res.status(500).json({ error: 'Erro ao buscar servidores por empresa' });
